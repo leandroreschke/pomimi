c918a6d1e0c278462b51cabf681ee925fab17707
Leandro Reschke <leandro.reschke@gmail.com>
2026-01-29 05:51:49 -0300

    add new iced 14

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,14 @@ fn main() -> iced::Result {
         cli::run();
         Ok(())
     } else {
-        iced::application(PomimiApp::title, PomimiApp::update, PomimiApp::view)
+        iced::application(PomimiApp::new, PomimiApp::update, PomimiApp::view)
+            .title(PomimiApp::title)
             .theme(PomimiApp::theme)
             .subscription(PomimiApp::subscription)
             .window(iced::window::Settings {
                 size: iced::Size::new(380.0, 800.0),
                 ..Default::default()
             })
-            .run_with(PomimiApp::new)
+            .run()
     }
 }
